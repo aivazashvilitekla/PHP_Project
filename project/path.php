787b1e280889776d5b22beab4632bfff1e71918b
aivazashvilitekla <48378815+aivazashvilitekla@users.noreply.github.com>
2020-06-03 20:23:59 +0400

    Project

--- project/path.php
+++ project/path.php
@@ -1,4 +1,6 @@
 <?php
     define("ROOT_PATH", realpath(dirname(__FILE__)));
-    define("BASE_URL", "http://localhost//PHP_Project/project");
+    // define("BASE_URL", "http://localhost/PHP_Project/project");
+    define("BASE_URL", 'http://localhost:'.$_SERVER['SERVER_PORT'].'/PHP_Project/project');
+
 ?>
